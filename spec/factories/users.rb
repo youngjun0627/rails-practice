5d3aed99b9c3612096a8f54087db6f683434d64d
uchanLee <yj2221@naver.com>
2022-03-14 22:40:46 +0900

    test 추가

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :user do
-    admin { false }
+    sequence :email do |n|
+      "test#{n}@grepp.co"
+    end
+    password {"1q2w3e4r"}
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,11 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  let!(:user) {FactoryBot.create(:user, )}
-  before do
-
-  end
-
+  let!(:user) {FactoryBot.create(:user )}
   describe 'login' do
     it '정상적으로 로그인 할 수 있다.' do
       puts user
